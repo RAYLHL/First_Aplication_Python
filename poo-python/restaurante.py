132bf6eb0dacabd3f1d4169d2dfdcc964c5253d7
Rayl Henrique <rayltsonhenrique04@gmail.com>
2025-08-19 19:59:39 -0300

    add: exericicios3

---
+++ poo-python/restaurante.py
@@ -0,0 +1,12 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_ifood = Restaurante()
+restaurante_KILO = Restaurante()
+
+restaurantes = [restaurante_ifood, 
+                restaurante_KILO]
+
+print(restaurantes)
