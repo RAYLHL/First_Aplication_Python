df7366ae9d6ed8fb94def441296beb143db199d7
Rayl Henrique <rayltsonhenrique04@gmail.com>
2025-09-08 18:59:48 -0300

    feat: Modificações na aplicação em geral

--- poo-python/modelos/restaurante.py
+++ poo-python/modelos/restaurante.py
@@ -3,8 +3,8 @@ from modelos.avaliacao import Avaliacao
 class Restaurante:
     restaurantes = []
     def __init__(self, nome , categoria): 
-        self.nome = nome
-        self.categoria = categoria
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
         self._ativo = False
         self._avaliacao = []
         Restaurante.restaurantes.append(self)
@@ -13,9 +13,9 @@ class Restaurante:
     
     @classmethod
     def listar_restaurante(cls):
-        print(f'{'Nome do resrtaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do resrtaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliacao'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes: 
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}' )
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacao).ljust(25)} | {restaurante.ativo}' )
 
     @property
     def ativo(self):
@@ -27,12 +27,13 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
-
-
+ 
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return '-' 
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return  media
     
-
-restaurante_ifood = Restaurante('ifood','Comida de aplicativo') 
-restaurante_ifood.alterar_estado()
-restaurante_KILO = Restaurante('KILO','Comida de Restaurante')
-
-Restaurante.listar_restaurante()
