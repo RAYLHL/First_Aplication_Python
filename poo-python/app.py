d104a5429d5d617fff2f4ce6de7063ee10066632
RAYLHL <rayltsonhenrique04@gmail.com>
2025-10-30 18:15:14 -0300

    feat: Continuação da Aplicação

--- poo-python/app.py
+++ poo-python/app.py
@@ -5,10 +5,13 @@ from modelos.cardapio.bebida import Bebida
 restaurante_japones = Restaurante('Japa',' Comida Japonesa ')
 bebida_suco = Bebida('Suco de Laranja', 8.00, '300ml')
 prato_shushi = Prato('Sushi', 25.00, 'Sushi de Salmão com arroz e alga')
+prato_macarrao = Prato('Macarrão',10.00, 'Macarrão ao molho')
+restaurante_japones.adicionar_no_cardapio(bebida_suco)
+restaurante_japones.adicionar_no_cardapio(prato_shushi)
+
 
 def main():
-    print(bebida_suco)
-    print(prato_shushi)
+   restaurante_japones.exibir_cardapio
 
 if __name__ == '__main__':
     main()
