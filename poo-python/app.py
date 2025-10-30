146bf201b230371c727b4cc5a3332e70cb7b1d83
RAYLHL <rayltsonhenrique04@gmail.com>
2025-10-30 16:49:35 -0300

    feat: Mudanças na Main

--- poo-python/app.py
+++ poo-python/app.py
@@ -1,10 +1,14 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
 
 restaurante_japones = Restaurante('Japa',' Comida Japonesa ')
-restaurante_japones.receber_avaliacao('Rayl', 9)
+bebida_suco = Bebida('Suco de Laranja', 8.00, '300ml')
+prato_shushi = Prato('Sushi', 25.00, 'Sushi de Salmão com arroz e alga')
 
 def main():
-    Restaurante.listar_restaurante()
+    print(bebida_suco)
+    print(prato_shushi)
 
 if __name__ == '__main__':
     main()
